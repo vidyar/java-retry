62fd288c2cca91808cac3ff16fecd18241a9184b
Manish Baxi <baxi.manish@welue.com>
2015-11-06 12:40:17 +0530

    Initial commit.

---
+++ src/main/scala/org/example/Calculator.scala
@@ -0,0 +1,7 @@
+package org.example
+
+object Calculator {
+  def minus(a: Int, b: Int) = a - b
+
+  def plus(a: Int, b: Int) = a + b
+}
---
+++ src/test/scala/org/example/CalculatorSpec.scala
@@ -0,0 +1,26 @@
+package org.example
+
+import java.security.SecureRandom
+
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CalculatorSpec extends FlatSpec with BeforeAndAfterEach with Inside with Matchers with OptionValues {
+  val RANDOM = new SecureRandom
+
+  "Two integers" can "be added correctly" in {
+    val a = RANDOM.nextInt
+    val b = RANDOM.nextInt
+
+    assert(Calculator.plus(a, b) == (a + b))
+  }
+
+  "Two integers" can "be subtracted correctly" in {
+    val a = RANDOM.nextInt
+    val b = RANDOM.nextInt
+
+    assert(Calculator.minus(a, b) == (a - b))
+  }
+}
